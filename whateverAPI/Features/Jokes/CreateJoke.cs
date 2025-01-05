af63d02c11ca3aedb58441ac6a8281b5f70c8941
Andrew Gunn <andrewgunn31@gmail.com>
2025-01-04 18:00:35 -0600

    Refactor joke creation endpoint
    - Renamed the class for clarity
    - Updated validation rules for content and tags
    - Removed an empty class that was redundant

--- whateverAPI/Features/Jokes/CreateJoke.cs
+++ whateverAPI/Features/Jokes/CreateJoke.cs
@@ -6,7 +6,7 @@ using whateverAPI.Services;
 
 namespace whateverAPI.Features.Jokes;
 
-public class CreateJoke2 : Endpoint<CreateJoke.Request, JokeResponse>
+public class CreateJoke : Endpoint<CreateJoke.Request, JokeResponse>
 {
     private readonly IJokeService _jokeService;
 
@@ -30,15 +30,15 @@ public class CreateJoke2 : Endpoint<CreateJoke.Request, JokeResponse>
             RuleFor(x => x.Content)
                 .NotEmpty().WithMessage("Content is required")
                 .MinimumLength(5).WithMessage("Content must be at least 10 characters");
-            
+
             RuleFor(x => x.Type)
                 .IsInEnum().WithMessage("Invalid joke type");
-            
+
             RuleFor(x => x.Tags)
                 .Must(tags => tags?.Count <= 10)
                 .WithMessage("Maximum 5 tags allowed")
                 .When(x => x.Tags != null);
-            
+
             RuleForEach(x => x.Tags)
                 .MaximumLength(20)
                 .WithMessage("Tag length cannot exceed 20 characters")
