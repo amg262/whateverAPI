b9b9be08daaf67ce6f5a50a35f33464de4496865
Andrew Gunn <andrewgunn31@gmail.com>
2025-01-24 18:41:32 -0600

    fixing db

--- whateverAPI/Data/Migrations/20250125004120_Initial.Designer.cs
+++ whateverAPI/Data/Migrations/20250125004120_Initial.Designer.cs
@@ -12,7 +12,7 @@ using whateverAPI.Data;
 namespace whateverAPI.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250124143710_Initial")]
+    [Migration("20250125004120_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
